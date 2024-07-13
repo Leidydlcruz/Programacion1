20c329891c11e023a9580c17a75a3fed5da23452
Leidy de la Cruz <leidydelacruzac@gmail.com>
2024-07-13 12:32:15 -0500

    Ejercicio de variables

---
+++ 04-variables.py
@@ -0,0 +1,39 @@
+# nombre = "Leidy"
+# print(nombre)
+
+# edad = 30
+# print(edad)
+
+# edad1 = 20
+# edad2 = 15
+# suma = edad1 + edad2
+# print(suma)
+
+# nombre = input("Dime tu nombre: ")
+# print("Tu nombre es: " + nombre)
+
+# primeNombre = "Leidy"
+# segundoNombre = "Elizabeth"
+# nombres = primeNombre + " " + segundoNombre
+# print(nombres)
+
+nombre1 = "Tony"
+nombre2 = "Soprano"
+nombres = nombre1 + " " + nombre2
+print(nombres)
+ 
+edad= 51
+print(edad)
+
+nombre = "Julia"
+apellido = "Roberts"
+nombreCompleto = nombre + " " + apellido
+print(nombreCompleto)
+
+Curso = "Python"
+print("Estas tomando un curso de " + Curso)
+
+
+
+
+
