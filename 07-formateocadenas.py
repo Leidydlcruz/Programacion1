a35303f47670e4ffd61492440bb11795c1d8f1aa
Leidy de la Cruz <leidydelacruzac@gmail.com>
2024-07-13 13:21:52 -0500

    Ejercicios clase 3

--- 07-formateocadenas.py
+++ 07-formateocadenas.py
@@ -1,4 +1,9 @@
-x = 10
-y = 5
+# x = 10
+# y = 5
 
-print("Mis numeros son {} y {}".format(x,y))
+# print("Mis numeros son {} y {}".format(x,y))
+# print("Mis numeros son {} y {}".format(y,x))
+
+color = "rojo"
+matricula = 12345
+print(f"El auto es {color} y su matricula es {matricula}")
